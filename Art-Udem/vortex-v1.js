9490085c4ff7ee988d5a7f67f3d532c23331e407
Jessey Thach <94651491+jesseythach@users.noreply.github.com>
2026-01-21 01:52:05 -0500

    Noise function and background

--- Art-Udem/vortex-v1.js
+++ Art-Udem/vortex-v1.js
@@ -8,31 +8,52 @@ function draw() {
   noFill();
   rect(0, 0, 800, 500);
 
-  // Parent circle
+  // Random RGB values
+  let randR = random(0, 255);
+  let randG = random(0, 255);
+  let randB = random(0, 255);
+
+  // Parent circle (random color)
   let parentX = 400;
   let parentY = 250;
   let parentRadius = 150;
-  fill(200, 0, 0);
+  noStroke()
+  fill(randR, randG, randB);
   circle(parentX, parentY, parentRadius * 2);
 
-  // Loop for 3 circles
-  for (let i = 0; i < 3; i++) {
-    let randAngle = random(0, 360); // Select a random angle
+  // Child circle
+  let childRadius;
+  let childMinProp = 0.6;
+  let childMaxProp = 0.9;
+  let nbChild = random(2, 11);
+
+  // Loop for nb_child circles
+  for (let i = 0; i < nbChild; i++) {
+    childRadius = random(
+      parentRadius * childMinProp,
+      parentRadius * childMaxProp,
+    ); // Select a random radius for child
+    let randAngle = random(0, 360); // Selects a random angle
+    randR = random(0, 255);
+    randG = random(0, 255);
+    randB = random(0, 255);
 
     // Calculate the horizontal and vertical steps to position child
     let angle = radians(randAngle);
-    let hypotenuse = parentRadius / 2;
+    let hypotenuse = parentRadius - childRadius;
     let stepX = hypotenuse * cos(angle);
     let stepY = hypotenuse * sin(angle);
 
-    // Child circle (diameter half of its parent)
+    // Child circle (random radius and color)
     let childX = parentX + stepX;
     let childY = parentY + stepY;
-    circle(childX, childY, parentRadius);
+    noStroke()
+    fill(randR, randG, randB);
+    circle(childX, childY, childRadius * 2);
 
     // Update variables
     parentX = childX;
     parentY = childY;
-    parentRadius /= 2;
+    parentRadius = childRadius;
   }
 }
--- Art-Udem/vortex-v2.js
+++ Art-Udem/vortex-v2.js
@@ -1,32 +1,38 @@
 function setup() {
-  createCanvas(800, 500);
+  createCanvas(1000, 900);
+  noLoop();
+  colorMode(HSB);
 }
 
-function draw() {
-  noLoop();
-  background("white");
-  noFill();
-  rect(0, 0, 800, 500);
+const vortexRadius = 45;
+
+function drawVortex(parentX, parentY) {
+  // Random HSB values
+  let hue = random(0, 360);
+  let sat = 80;
+  let bri = 85;
 
   // Parent circle
-  let parentX = 400;
-  let parentY = 250;
-  let parentRadius = 150;
-  fill(200, 0, 0);
+  let parentRadius = vortexRadius;
+  fill(hue, sat, bri);
   circle(parentX, parentY, parentRadius * 2);
 
   // Child circle
   let childRadius;
   let childMinProp = 0.6;
   let childMaxProp = 0.9;
+  let nbChild = random(2, 11);
 
-  // Loop for 3 circles
-  for (let i = 0; i < 3; i++) {
+  // Loop for nb_child circles
+  for (let i = 0; i < nbChild; i++) {
     childRadius = random(
       parentRadius * childMinProp,
       parentRadius * childMaxProp,
     ); // Select a random radius for child
     let randAngle = random(0, 360); // Selects a random angle
+    hue = random(0, 360);
+    sat = 80;
+    bri = 85;
 
     // Calculate the horizontal and vertical steps to position child
     let angle = radians(randAngle);
@@ -34,9 +40,10 @@ function draw() {
     let stepX = hypotenuse * cos(angle);
     let stepY = hypotenuse * sin(angle);
 
-    // Child circle (random radius)
+    // Child circle (random radius and color)
     let childX = parentX + stepX;
     let childY = parentY + stepY;
+    fill(hue, sat, bri);
     circle(childX, childY, childRadius * 2);
 
     // Update variables
@@ -44,4 +51,64 @@ function draw() {
     parentY = childY;
     parentRadius = childRadius;
   }
+}
+
+function colorGreenRect() {
+  let hue = random(95, 150);
+  let sat = random(25, 35);
+  let bri = random(80, 100);
+  let alpha = 80;
+  fill(hue, sat, bri, alpha);
+}
+
+function drawRectangles() {
+  // Random number between 0 and 1 (exclusive)
+  let h1 = random();
+  let h2 = random();
+  let h3 = random();
+  let total = h1 + h2 + h3;
+
+  // Normalize
+  h1 = (h1 / total) * height;
+  h2 = (h2 / total) * height;
+  h3 = (h3 / total) * height;
+
+  let y = 0;
+  colorGreenRect();
+  rect(0, y, width, h1);
+  y += h1;
+  colorGreenRect();
+  rect(0, y, width, h2);
+  y += h2;
+  colorGreenRect();
+  rect(0, y, width, h3);
+}
+
+function drawCurve() {
+  let incrementY = 5; // Increment in y
+  let noiseScale = 0.002; // Adjust curviness
+  let amplitude = width;
+  let gapSquared = (vortexRadius * 2.1) ** 2; // Squared distance between two vortexes
+  let prevX = 0;
+  let prevY = 0;
+
+  for (let y = 0; y <= height; y += incrementY) {
+    // Multiply by amplitude and add an offset to center it horizontally
+    let x = noise(y * noiseScale) * amplitude - amplitude / 2 + width / 2;
+
+    // Draw vortex if gap is large enough
+    if ((x - prevX) ** 2 + (y - prevY) ** 2 > gapSquared) {
+      drawVortex(x, y, vortexRadius);
+      prevX = x;
+      prevY = y;
+    }
+  }
+}
+
+function draw() {
+  background(255);
+  noStroke();
+
+  drawRectangles();
+  drawCurve();
 }
